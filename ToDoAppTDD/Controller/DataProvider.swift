8def84e1f1ad82ea6c8e066ffc3e2908e0b2849e
Artem Tkachev <tkachevforios@icloud.com>
2023-11-07 12:50:17 +0300

    TaskListVC+tests

---
+++ ToDoAppTDD/Controller/DataProvider.swift
@@ -0,0 +1,28 @@
+//
+//  DataProvider.swift
+//  ToDoAppTDD
+//
+//  Created by Artem Tkachev on 7.11.23.
+//
+
+import UIKit
+
+class DataProvider: NSObject {
+    
+}
+
+extension DataProvider: UITableViewDelegate {
+    
+}
+
+extension DataProvider: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
---
+++ ToDoAppTDDTests/Controller/TaskListViewControllerTests.swift
@@ -0,0 +1,46 @@
+//
+//  TaskListViewControllerTests.swift
+//  ToDoAppTDDTests
+//
+//  Created by Artem Tkachev on 5.11.23.
+//
+
+import XCTest
+@testable import ToDoAppTDD
+
+final class TaskListViewControllerTests: XCTestCase {
+    
+    var sut: TaskListViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self))
+        sut = vc as? TaskListViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testWhenViewIsLoadedTableViewNotNil() {
+        XCTAssertNotNil(sut.tableView)
+    }
+    
+    func testWhenViewIsLoadedDataProviderIsNotNil() {
+        XCTAssertNotNil(sut.dataProvider)
+    }
+    
+    func testWhenViewIsLoadedTableViewDelegateIsSet() {
+        XCTAssertTrue(sut.tableView.delegate is DataProvider)
+    }
+    
+    func testWhenViewIsLoadedTableViewDataSourceIsSet() {
+        XCTAssertTrue(sut.tableView.dataSource is DataProvider)
+    }
+    
+    func testWhenViewIsLoadedTableViewDelegateEqualsTableViewDataSourceIsSet(){
+        XCTAssertEqual(sut.tableView.delegate as? DataProvider, sut.tableView.dataSource as? DataProvider)
+    }
+
+}
